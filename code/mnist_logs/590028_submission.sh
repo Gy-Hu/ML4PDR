bdaf4effed4bfad45991944c414a9449d337c50e
Guangyu HU <ghuae@connect.ust.hk>
2022-03-09 18:57:41 +0800

    update slurm branch

---
+++ code/mnist_logs/590028_submission.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Parameters
+#SBATCH --cpus-per-task=4
+#SBATCH --error=/home/ghuae/coding_env/ML4PDR/code/mnist_logs/%j_0_log.err
+#SBATCH --job-name=submitit
+#SBATCH --mem=1GB
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --open-mode=append
+#SBATCH --output=/home/ghuae/coding_env/ML4PDR/code/mnist_logs/%j_0_log.out
+#SBATCH --partition=gpu-share
+#SBATCH --signal=USR1@90
+#SBATCH --time=5
+#SBATCH --wckey=submitit
+
+# command
+export SUBMITIT_EXECUTOR=slurm
+srun --output /home/ghuae/coding_env/ML4PDR/code/mnist_logs/%j_%t_log.out --error /home/ghuae/coding_env/ML4PDR/code/mnist_logs/%j_%t_log.err --unbuffered /home/ghuae/miniconda3/envs/pytorch-gpu/bin/python -u -m submitit.core._submit /home/ghuae/coding_env/ML4PDR/code/mnist_logs
---
+++ code/test_submitit_logs/590029_submission.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Parameters
+#SBATCH --error=/home/ghuae/coding_env/ML4PDR/code/test_submitit_logs/%j_0_log.err
+#SBATCH --job-name=submitit
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --open-mode=append
+#SBATCH --output=/home/ghuae/coding_env/ML4PDR/code/test_submitit_logs/%j_0_log.out
+#SBATCH --partition=gpu-share
+#SBATCH --signal=USR1@90
+#SBATCH --time=5
+#SBATCH --wckey=submitit
+
+# command
+export SUBMITIT_EXECUTOR=slurm
+srun --output /home/ghuae/coding_env/ML4PDR/code/test_submitit_logs/%j_%t_log.out --error /home/ghuae/coding_env/ML4PDR/code/test_submitit_logs/%j_%t_log.err --unbuffered /home/ghuae/miniconda3/envs/pytorch-gpu/bin/python -u -m submitit.core._submit /home/ghuae/coding_env/ML4PDR/code/test_submitit_logs
